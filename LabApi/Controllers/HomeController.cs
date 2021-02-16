cba1fd975b4db2b8eada84c424980e865a61ac76
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2021-02-16 02:27:24 -0800

    Trying to use certificate

---
+++ LabApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return Json(new { 
+                Mesage = "HI",
+                IsHttps = Request.IsHttps
+            });
+        }
+    }
+}
--- LabApi/Startup.cs
+++ LabApi/Startup.cs
@@ -26,7 +26,9 @@ namespace LabApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddRouting(options => { 
+                
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
